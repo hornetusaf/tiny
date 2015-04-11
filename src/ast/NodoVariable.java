619669bca5bc5db092c9709e9f2dfd1238f94e5c
silbon28 <bonilla960@gmail.com>
2015-04-10 20:50:30 -0430

    Agrego TRUE y FALSE modificacion NodoVariable

--- src/ast/NodoVariable.java
+++ src/ast/NodoVariable.java
@@ -3,29 +3,34 @@ package ast;
 public class NodoVariable extends NodoBase{
 	
 	private String id;
-	private NodoBase partev;	
+	private NodoBase partev;
+	private String tipo;
 
-	public NodoVariable(String id) {
+	public NodoVariable(String id,String tipo) {
 		super();
 		this.id = id;
 		this.partev = null;
+		this.tipo= tipo;
 	}
-	public NodoVariable(NodoBase partev) {
+	public NodoVariable(NodoBase partev,String tipo) {
 		super();
 		this.id = null;
 		this.partev = partev;
+		this.tipo = tipo;
 	}
 	
-	public NodoVariable(String id, NodoBase partev) {
+	public NodoVariable(String id, NodoBase partev,String tipo) {
 		super();
 		this.id = id;
 		this.partev = partev;
+		this.tipo = tipo;
 	}
 	
 	public NodoVariable() {
 		super();
 		this.id = null;
 		this.partev = null;
+		this.tipo = null;
 	}
 	
 	public String getId() {
@@ -34,6 +39,14 @@ public class NodoVariable extends NodoBase{
 
 	public void setId(String id) {
 		this.id = id;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 
 	public NodoBase getPartev() {
