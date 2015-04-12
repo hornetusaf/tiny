ee8cd5a6b7d0b2e42b830c722b430b68857a4677
silverio <bonilla960@gmail.com>
2015-04-12 08:58:57 -0430

    actualizando branch silverio
    actualizando branch silverio

---
+++ src/ast/NodoFor.java
@@ -0,0 +1,60 @@
+package ast;
+
+public class NodoFor extends NodoBase{
+
+	private NodoBase asi;
+	private NodoBase asf;
+	private NodoBase exp;
+	private NodoBase cuerpo;
+	
+	public NodoFor() {
+		super();
+		this.asi = null;
+		this.asf = null;
+		this.exp = null;
+		this.cuerpo = null;	
+	}
+
+	public NodoFor(NodoBase asi, NodoBase exp, NodoBase asf, NodoBase cuerpo) {
+		super();
+		this.asi = asi;
+		this.exp = exp;
+		this.asf = asf;
+		this.cuerpo = cuerpo;
+		
+	}
+	
+
+
+	public NodoBase getAsi() {
+		return asi;
+	}
+
+	public void setAsi(NodoBase asi) {
+		this.asi = asi;
+	}
+	
+	public NodoBase getAsf() {
+		return asf;
+	}
+
+	public void setAsf(NodoBase asf) {
+		this.asf = asf;
+	}
+	
+	public NodoBase getExp() {
+		return exp;
+	}
+
+	public void setExp(NodoBase exp) {
+		this.exp = exp;
+	}
+	
+	public NodoBase getCuerpo() {
+		return cuerpo;
+	}
+
+	public void setCuerpo(NodoBase cuerpo) {
+		this.cuerpo = cuerpo;
+	}
+}
---
+++ src/ast/NodoVector.java
@@ -0,0 +1,46 @@
+package ast;
+
+public class NodoVector extends NodoBase{
+
+	private String id;
+	private tipoDato tipo;
+	private Integer tam;
+	
+	public NodoVector()	{
+		super();
+		this.id = null;
+		this.tipo = null;
+		this.tam = 0;		
+	}
+	
+	public NodoVector(String id, tipoDato tipo, Integer tam)	{
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.tam = tam;		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public tipoDato getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(tipoDato tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getTam() {
+		return tam;
+	}
+
+	public void setTam(Integer tam) {
+		this.tam = tam;
+	}	
+}
