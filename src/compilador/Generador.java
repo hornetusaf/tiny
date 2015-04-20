f0d609297df7b8b4834e20265214467a54f7db5c
yoel28 <yo3l.m18@gmail.com>
2015-04-19 22:52:04 -0430

    operacion en el generador

--- src/compilador/Generador.java
+++ src/compilador/Generador.java
@@ -152,9 +152,15 @@ public class Generador {
 			{
 				RegistroSimbolo var=tablaSimbolos.BuscarSimbolo(ambito+"."+((NodoIdentificador)((NodoCall)a).getExI()).getNombre());
 				UtGen.emitirRM("LDC", UtGen.AC1,0 , 0, "cargar Valor 0");				
-				UtGen.emitirRM("LDA", UtGen.AC, var.getDireccionMemoria(),UtGen.AC1, "Descargando Valor de: "+ambito+"."+((NodoIdentificador)((NodoCall)a).getExI()).getNombre());
+				UtGen.emitirRM("LD", UtGen.AC, var.getDireccionMemoria(),UtGen.AC1, "Descargando Valor de: "+ambito+"."+((NodoIdentificador)((NodoCall)a).getExI()).getNombre());
 				UtGen.emitirRM("ST", UtGen.AC, tablaSimbolos.getDireccion(((NodoCall)nodo).getNombreFuncion())+cont, UtGen.AC1,"subiendo posicion de memoria identificador");							
-			}			
+			}
+			else if(((NodoCall)a).getExI() instanceof NodoOperacion)
+			{				
+				generarOperacion(((NodoCall)a).getExI(), ambito);
+				UtGen.emitirRM("LDC", UtGen.AC1,0 , 0, "cargar Valor 0");
+				UtGen.emitirRM("ST", UtGen.AC, tablaSimbolos.getDireccion(((NodoCall)nodo).getNombreFuncion())+cont, UtGen.AC1,"subiendo posicion de memoria identificador");
+			}
 			cont--;
 			a=((NodoCall)a).getExD();
 		}
